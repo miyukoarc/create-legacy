c6b82c2221aa4ffc6550d8ced16f20d47e632afd
oscar <oscarcgm@hotmail.com>
2023-01-10 19:12:04 +0800

    fix: 构建配置

--- template/complex/vite.config.ts
+++ template/complex/vite.config.ts
@@ -3,13 +3,33 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
 import vueJsx from '@vitejs/plugin-vue2-jsx'
+import windiCSS from 'vite-plugin-windicss'
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    windiCSS(),
+    viteMockServe({
+      mockPath: './mock',
+      localEnabled: true,
+      prodEnabled: false,
+      watchFiles: true,
+      supportTs: true
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/assets/style/variables.scss" as *;`
+      }
     }
   }
 })
